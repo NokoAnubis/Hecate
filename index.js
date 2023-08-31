129f886262e3df167a26033a836e69c7202609f2
Joel Joseph <joeljordanjoseph@gmail.com>
2023-08-30 19:26:07 -0600

    files re-arranged. ready for packaging

---
+++ index.js
@@ -0,0 +1,11 @@
+import Method from './src/method';
+import Courrier from './src/service';
+import Endpoint from './src/endpoint';
+import QueryItem from './src/queryItem';
+
+module.exports = {
+    Method,
+    Courrier,
+    Endpoint,
+    QueryItem
+}
