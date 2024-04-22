af5a2f48f861cae02a08d808669a34adfb2ac82d
josephjoeljo <joeljordanjoseph@gmail.com>
2024-04-22 14:18:03 -0600

    fixed exports

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
-export { Method } from './src/method';
-export { Courrier } from './src/service';
-export { Endpoint }  from './src/endpoint';
-export { FileType } from './src/fileType';
-export { Scheme } from './src/scheme';
-export * from './src/types';
+import { Method } from './src/method';
+import { Courrier } from './src/service';
+import { Endpoint }  from './src/endpoint';
+import { FileType } from './src/fileType';
+import { Scheme } from './src/scheme';
+
+export {
+    Method,
+    Courrier,
+    Endpoint,
+    FileType,
+    Scheme
+}
