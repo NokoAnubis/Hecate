0198dceb9ef5e2e0f26167aabf605a9795ea3829
josephjoeljo <joeljordanjoseph@gmail.com>
2024-04-22 11:35:30 -0600

    changed name of package and updated exports for the index file

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 export { Method } from './src/method';
 export { Courrier } from './src/service';
 export { Endpoint }  from './src/endpoint';
+export { FileType } from './src/fileType';
+export { Scheme } from './src/scheme';
 export * from './src/types';
